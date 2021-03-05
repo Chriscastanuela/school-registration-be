02f9f2ea559855ee1eda8fd1cf214472465fbf2e
Chriscastanuela <chriscastanuela@ymail.com>
2021-03-05 14:43:17 -0700

    add single student and all student return

--- server.js
+++ server.js
@@ -13,3 +13,14 @@ app.locals.courses = [
     {id: 001, name: '', time: 03/30/21},
 ]
 
+app.get('/api/v1/students', (request, response) => {
+    const students = app.locals.students;
+    response.json({ students });
+});
+
+app.get('/api/v1/students:id', (request, response) => {
+    const { id } = request.params;
+    const student = app.locals.students.find(student => student.id === id);
+
+    response.status(200).json(student);
+});
