06247a897647dea28e5dd5df174dacc25c1f3657
Chriscastanuela <chriscastanuela@ymail.com>
2021-03-05 14:16:31 -0700

    add student and course

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', 3000);
+
+app.locals.title = 'School registration site backend';
+
+app.locals.students = [
+    {id: 001, firstName: 'Ruth', lastName: 'Bader-Ginsburg', email: 'rbg1@gmail.com', courses: []},
+]
+
+app.locals.courses = [
+    {id: 001, name: '', time: 03/30/21},
+]
+
